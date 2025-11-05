aadf353d0068108a8c845f09367ace5411fe64f1
Adeoti Israel <adeotiisrael2022@gmail.com>
2025-11-05 21:27:46 +0100

    Fix vite dependency for Vercel build

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://10.172.210.31:5000/api"  // <-- your phone's IP
+const api = axios.create({
+  baseURL: "https://workbridge-backend-api.onrender.com", // your live backend base URL
 });
 
-API.interceptors.request.use(config => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
-
-export default API;
+export default api;
