781d65036773eae032bd5eef769bb48b5b2090de
Adeoti Israel <110563566+Smartnaka@users.noreply.github.com>
2025-11-06 23:23:22 +0100

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,27 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const token = localStorage.getItem("token");
+  const user = localStorage.getItem("user");
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) setIsAuthenticated(true);
-    setLoading(false);
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <p className="text-lg text-text-secondary-light dark:text-text-secondary-dark">
-          Checking authentication...
-        </p>
-      </div>
-    );
+  if (!token || !user) {
+    return <Navigate to="/login" replace />;
   }
-
-  if (!isAuthenticated) return <Navigate to="/login" replace />;
 
   return children;
 };
