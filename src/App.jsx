d10a72b9dab62aba8bad539bf72947f2a706a8df
Adeoti Israel <110563566+Smartnaka@users.noreply.github.com>
2025-11-06 13:50:32 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
-import './styles/tailwind.css'; // make sure this path points to your Tailwind CSS file
 
 function App() {
-  return (
-    <>
-      {/* Tailwind test div */}
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
-        <div className="bg-blue-500 text-white p-6 rounded shadow-lg text-center">
-          Tailwind is working!
-        </div>
-      </div>
-
-      {/* Your existing app routes */}
-      <AppRoutes />
-    </>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
