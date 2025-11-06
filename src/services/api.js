deea1284dad5e858fd1b8987aec40a74ff4628ff
Adeoti Israel <110563566+Smartnaka@users.noreply.github.com>
2025-11-06 10:17:38 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://workbridge-backend-api.onrender.com", // your live backend base URL
+  baseURL: "https://workbridge-backend-api.onrender.com/", // your live backend base URL
 });
 
 export default api;
